4ca3e1c2c9a325909126dbc8d7721886ca190b5d
Hocine Benzagouta <hocine.benzagouta@gmail.com>
2021-01-30 21:23:24 +0100

    add profile model

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterform(UserCreationForm):
     email = forms.EmailField()
-
+    #for styling
+    #email = forms.EmailField(widget=forms.TextInput(attrs={'class':'btn btn-primary','value':'hello world'}))
+    
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
--- users/views.py
+++ users/views.py
@@ -12,10 +12,6 @@ def register(request):
         password= form.cleaned_data.get('password')
         messages.success(request,f'Account created for {username} !, now you can log in')    
         return redirect('login')
-
-     # messages.warning(request,f'Something wrong')
-     #return redirect('register')
-
     else:    
       form = UserRegisterform()
     return render(request,'users/register.html',{'form' : form})
